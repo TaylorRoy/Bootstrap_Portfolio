d5872e3d7fc701ec63605a77c34fc90896e2baef
TaylorRoy <tbroy@hotmail.com>
2018-09-17 17:13:54 -0600

    updated portfolio and about page

---
+++ javascript/portfolio.js
@@ -0,0 +1,23 @@
+// $(document).on("click", ".word-guess", function(){
+//     window.open("https://taylorroy.github.io/GIPHY-API/")
+// });
+$(document).on("click", ".hangman", function(){
+    window.open("https://taylorroy.github.io/Psychic_Game/")
+});
+$(document).on("click", ".trivia", function(){
+    
+    window.open("https://taylorroy.github.io/TriviaGame/")
+});
+$(document).on("click", ".crystal", function(){
+   
+    window.open("https://taylorroy.github.io/unit-4-game/")
+});
+$(document).on("click", ".giphy", function(){
+
+    window.open("https://taylorroy.github.io/GIPHY-API/")
+});
+
+$(document).on("click", ".train-scheduler", function(){
+
+    window.open("https://taylorroy.github.io/Train-Scheduler/")
+});
