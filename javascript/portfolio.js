29d0ed71b9b579e21dc0689b96c99015af1c76aa
TaylorRoy <tbroy@hotmail.com>
2018-09-29 21:17:42 -0600

    added soda guru and linkedin link

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -2,22 +2,27 @@
 //     window.open("https://taylorroy.github.io/GIPHY-API/")
 // });
 $(document).on("click", ".hangman", function(){
-    window.open("https://taylorroy.github.io/Psychic_Game/")
+    window.open("https://taylorroy.github.io/Psychic_Game/");
 });
 $(document).on("click", ".trivia", function(){
     
-    window.open("https://taylorroy.github.io/TriviaGame/")
+    window.open("https://taylorroy.github.io/TriviaGame/");
 });
 $(document).on("click", ".crystal", function(){
    
-    window.open("https://taylorroy.github.io/unit-4-game/")
+    window.open("https://taylorroy.github.io/unit-4-game/");
 });
 $(document).on("click", ".giphy", function(){
 
-    window.open("https://taylorroy.github.io/GIPHY-API/")
+    window.open("https://taylorroy.github.io/GIPHY-API/");
 });
 
 $(document).on("click", ".train-scheduler", function(){
 
-    window.open("https://taylorroy.github.io/Train-Scheduler/")
+    window.open("https://taylorroy.github.io/Train-Scheduler/");
+});
+
+$(document).on("click", ".soda-guru", function(){
+    alert("you clicked")
+    window.open("https://taylorroy.github.io/Soda_Guru/");
 });
